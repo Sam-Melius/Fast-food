1bbe3bd6f9b09c3085acc07a5d3b454dd7413d02
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-31 18:35:13 -0700

    fixed instructions form, no errors but not rendering new instructions

--- src/InstructionsForm.js
+++ src/InstructionsForm.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { useState } from 'react/cjs/react.development';
 
 export default function InstructionsForm({ addInstructions, setAddInstructions }) {
-  const [instructionsInput, setInstructionsForm] = useState('');
+  const [instructionsInput, setInstructionsInput] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    addInstructions([...addInstructions, instructionsInput]);
-    setAddInstructions('');
+    setAddInstructions([...addInstructions, instructionsInput]);
+    setInstructionsInput('');
   }
 
   return <div>
     <form onSubmit={handleSubmit}>
         Additional Instructions:
-      <input value={instructionsInput} onChange={e => setAddInstructions(e.target)} />
+      <input value={instructionsInput} onChange={e => setInstructionsInput(e.target.value)} />
       <button>Submit</button>
     </form>
   </div>;
